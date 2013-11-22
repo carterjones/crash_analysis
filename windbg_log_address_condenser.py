1d3b709dd567d10e0f754eb91e9fdcdc6b7e8833
Carter Jones <carter@carterjones.info>
2013-11-21 21:07:16 -0800

    add cyclic value detector

--- windbg_log_address_condenser.py
+++ windbg_log_address_condenser.py
@@ -9,6 +9,60 @@ Outputs a list of addresses, along with log files that correspond to that
 crash address.
 '''
 
+def get_cyclic_rating(value):
+  '''
+  Rates the string with a value depending on how much repetition occurs in the
+  string.
+
+  The rating system is not intensley complex, but it ranks highly repetetive
+  strings very highly.
+  '''
+  if not isinstance(value, basestring):
+    return 0
+  if len(value) == 8 or len(value) == 16:
+    weight = 0
+    '''
+    # Look for repeated single characters.
+    for x in value:
+      # Check if it is repeated 16 times, then 8 times, then 4, etc.
+      if x * 16 in value:
+        weight += 8
+      elif x * 8 in value:
+        weight += 4
+      elif x * 4 in value:
+        weight += 2
+      elif x * 2 in value:
+        weight += 1
+    '''
+    # Look for repeated double characters.
+    pairs = [value[0:2], value[2:4], value[4:6], value[6:8]]
+    if len(value) == 16:
+      pairs = pairs + [value[8:10], value[10:12], value[12:14], value[14:16]]
+    for x in pairs:
+      # Check if it is repeated 8 times, then 4, etc.
+      if x * 8 in value:
+        weight += 8
+      elif x * 4 in value:
+        weight += 4
+      elif x * 2 in value:
+        weight += 2
+    # Look for repeated four characters.
+    fours = [value[0:4], value[4:8]]
+    if len(value) == 16:
+      fours = fours + [value[8:12], value[12:16]]
+    for x in fours:
+      # Check if it is repeated 4 times and then 2 times.
+      if x * 4 in value:
+        weight += 8
+      elif x * 2 in value:
+        weight += 4
+    # Return the weight.
+    return weight
+
+  else:
+    print '[-] ' + value + ' can not be rated for cyclic patterns'
+    return 0
+
 class CrashInfo:
   '''Information about a crash, obtained from the output of windbg'''
   def __init__(self):
@@ -23,6 +77,9 @@ class CrashInfo:
       return self.registers['rip']
     else:
       raise 'No instruction pointer was found.'
+
+  def score():
+    pass
 
 def usage(script_name):
   print 'usage: ' + script_name + ' <crash_log_directory>'
