5b356e58ed806b5ebe6f26b1bf9013ca5d2f3af3
Carter Jones <carter@carterjones.info>
2013-11-26 12:30:34 -0800

    only output necessary information from scripts

--- start_fuzzing.bat
+++ start_fuzzing.bat
@@ -3,10 +3,12 @@ set username=admin
 set password=1
 set ip=192.168.91.128
 
-psexec \\%ip% -u %username% -p %password% net share shared="C:\fuzzing_tools"
-net use z: \\%ip%\shared /user:%username% %password%
-copy start_minifuzz.au3 Z:\start_minifuzz.au3
-copy stop_minifuzz.au3 Z:\stop_minifuzz.au3
-net use z: /delete
-psexec \\%ip% -u %username% -p %password% net share shared /delete
-psexec \\%ip% -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\fuzzing_tools\start_minifuzz.au3"
+echo copying over start_minifuzz.au3 and stop_minifuzz.au3...
+psexec \\%ip% -u %username% -p %password% net share shared="C:\fuzzing_tools" >nul 2>&1
+net use z: \\%ip%\shared /user:%username% %password% >nul 2>&1
+copy start_minifuzz.au3 Z:\start_minifuzz.au3 >nul 2>&1
+copy stop_minifuzz.au3 Z:\stop_minifuzz.au3 >nul 2>&1
+net use z: /delete >nul 2>&1
+psexec \\%ip% -u %username% -p %password% net share shared /delete >nul 2>&1
+echo starting minifuzz...
+psexec \\%ip% -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\fuzzing_tools\start_minifuzz.au3" >nul 2>&1
--- stop_fuzzing.bat
+++ stop_fuzzing.bat
@@ -3,4 +3,5 @@ set username=admin
 set password=1
 set ip=192.168.91.128
 
-psexec \\%ip% -u %username% -p %password% -i "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\fuzzing_tools\stop_minifuzz.au3"
+echo stopping minifuzz...
+psexec \\%ip% -u %username% -p %password% -i "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\fuzzing_tools\stop_minifuzz.au3" >nul 2>&1
