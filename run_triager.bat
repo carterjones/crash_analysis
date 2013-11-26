897501ebd9109c18f197c39c7d30d269020c7165
Carter Jones <carter@carterjones.info>
2013-11-26 01:19:08 -0800

    remove extra parameter

--- run_triager.bat
+++ run_triager.bat
@@ -7,4 +7,4 @@ net use z: \\192.168.91.128\shared /user:%username% %password%
 copy triage.py Z:\triage.py
 net use z: /delete
 psexec \\192.168.91.128 -u %username% -p %password% net share shared /delete
-psexec \\192.168.91.128 -u %username% -p %password% -w C:\fuzzing_tools -i -w C:\fuzzing_tools C:\python27\python.exe C:\fuzzing_tools\triage.py "C:\Program Files\VideoLAN\VLC\vlc.exe" C:\minifuzz\crashes
+psexec \\192.168.91.128 -u %username% -p %password% -w C:\fuzzing_tools -i C:\python27\python.exe C:\fuzzing_tools\triage.py "C:\Program Files\VideoLAN\VLC\vlc.exe" C:\minifuzz\crashes
