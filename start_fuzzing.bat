0de17616c32353a2dc2d9d65e24f1c0197b3a98e
Carter Jones <carter@carterjones.info>
2013-11-25 23:16:04 -0800

    copy over autoit file prior to running

--- start_fuzzing.bat
+++ start_fuzzing.bat
@@ -1,1 +1,12 @@
-psexec \\192.168.91.128 -u admin -p 1 -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\Documents and Settings\admin\Desktop\fuzzing_tools\start_minifuzz.au3"
+@echo off
+set username=admin
+set password=1
+@echo on
+
+psexec \\192.168.91.128 -u %username% -p %password% net share shared="C:\Documents and Settings\admin\Desktop\fuzzing_tools"
+net use z: \\192.168.91.128\shared /user:%username% %password%
+copy start_minifuzz.au3 Z:\start_minifuzz.au3
+copy stop_minifuzz.au3 Z:\stop_minifuzz.au3
+psexec \\192.168.91.128 -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\Documents and Settings\admin\Desktop\fuzzing_tools\start_minifuzz.au3"
+net use z: /delete
+psexec \\192.168.91.128 -u %username% -p %password% net share shared /delete
--- stop_fuzzing.bat
+++ stop_fuzzing.bat
@@ -1,1 +1,6 @@
-psexec \\192.168.91.128 -u admin -p 1 -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\Documents and Settings\admin\Desktop\fuzzing_tools\stop_minifuzz.au3"
+@echo off
+set username=admin
+set password=1
+@echo on
+
+psexec \\192.168.91.128 -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\Documents and Settings\admin\Desktop\fuzzing_tools\stop_minifuzz.au3"
