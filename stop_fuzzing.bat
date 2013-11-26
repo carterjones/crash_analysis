71e2d80117ab8942d6bd3eaf483a8147564c295c
Carter Jones <carter@carterjones.info>
2013-11-26 08:32:05 -0800

    wait for execution to end

--- stop_fuzzing.bat
+++ stop_fuzzing.bat
@@ -3,4 +3,4 @@ set username=admin
 set password=1
 set ip=192.168.91.128
 
-psexec \\%ip% -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\fuzzing_tools\stop_minifuzz.au3"
+psexec \\%ip% -u %username% -p %password% -i "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\fuzzing_tools\stop_minifuzz.au3"
