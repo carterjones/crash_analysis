7ed588833c4c194321e1b4aea65baf845962e6af
Carter Jones <carter@carterjones.info>
2013-11-25 23:57:34 -0800

    rearange commands

--- start_fuzzing.bat
+++ start_fuzzing.bat
@@ -6,6 +6,6 @@ psexec \\192.168.91.128 -u %username% -p %password% net share shared="C:\Documen
 net use z: \\192.168.91.128\shared /user:%username% %password%
 copy start_minifuzz.au3 Z:\start_minifuzz.au3
 copy stop_minifuzz.au3 Z:\stop_minifuzz.au3
-psexec \\192.168.91.128 -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\Documents and Settings\admin\Desktop\fuzzing_tools\start_minifuzz.au3"
 net use z: /delete
 psexec \\192.168.91.128 -u %username% -p %password% net share shared /delete
+psexec \\192.168.91.128 -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\Documents and Settings\admin\Desktop\fuzzing_tools\start_minifuzz.au3"
