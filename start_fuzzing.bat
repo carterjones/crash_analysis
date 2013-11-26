92515d5fb67d79e35468ddca6cceb59335e17532
Carter Jones <carter@carterjones.info>
2013-11-26 00:35:27 -0800

    change directories for fuzzing tools

--- start_fuzzing.bat
+++ start_fuzzing.bat
@@ -2,10 +2,10 @@
 set username=admin
 set password=1
 
-psexec \\192.168.91.128 -u %username% -p %password% net share shared="C:\Documents and Settings\admin\Desktop\fuzzing_tools"
+psexec \\192.168.91.128 -u %username% -p %password% net share shared="C:\fuzzing_tools"
 net use z: \\192.168.91.128\shared /user:%username% %password%
 copy start_minifuzz.au3 Z:\start_minifuzz.au3
 copy stop_minifuzz.au3 Z:\stop_minifuzz.au3
 net use z: /delete
 psexec \\192.168.91.128 -u %username% -p %password% net share shared /delete
-psexec \\192.168.91.128 -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\Documents and Settings\admin\Desktop\fuzzing_tools\start_minifuzz.au3"
+psexec \\192.168.91.128 -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\fuzzing_tools\start_minifuzz.au3"
--- stop_fuzzing.bat
+++ stop_fuzzing.bat
@@ -2,4 +2,4 @@
 set username=admin
 set password=1
 
-psexec \\192.168.91.128 -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\Documents and Settings\admin\Desktop\fuzzing_tools\stop_minifuzz.au3"
+psexec \\192.168.91.128 -u %username% -p %password% -i -d "C:\Program Files\AutoIt3\AutoIt3.exe" "C:\fuzzing_tools\stop_minifuzz.au3"
