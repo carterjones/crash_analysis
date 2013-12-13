09b3de2a840a3e3568bbe9127aec91a0d736bb6f
Carter Jones <carter@carterjones.info>
2013-12-13 12:59:53 -0800

    enable automatic logon

--- DeployNode/run_on_node_first.bat
+++ DeployNode/run_on_node_first.bat
@@ -12,3 +12,11 @@ netsh firewall set opmode disable
 
 :: Disable simple file sharing.
 reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa /v forceguest /t reg_dword /d 0 /f
+
+:: Enable automatic logon.
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d admin /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
+
+:: Restart the computer.
+shutdown -r -t 0
