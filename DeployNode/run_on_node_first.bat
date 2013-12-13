8f2e276b655e9865defc9e65c20bbc054a986b80
Carter Jones <carter@carterjones.info>
2013-12-13 11:37:44 -0800

    create batch script for node deployment

---
+++ DeployNode/run_on_node_first.bat
@@ -0,0 +1,14 @@
+:: Create the admin without a password, in case the user already exists.
+net user /add admin
+
+:: Change the admin account password.
+net user admin 1
+
+:: Make the admin an actual administrator.
+net localgroup administrators admin /add
+
+:: Disable the firewall.
+netsh firewall set opmode disable
+
+:: Disable simple file sharing.
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa /v forceguest /t reg_dword /d 0 /f
